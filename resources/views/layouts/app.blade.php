aaccf2a7d697b1a6d4520ea483c4e05af396a22a
cesar rihu <cesarrihu@hotmail.com>
2023-09-05 18:29:35 -0600

    agregue bootstrap y guardare progreso antes de empezar a modificar apoyado de algo ya hecho

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PetFinder- @yield('title')</title>
     <meta name="description" content="@yield('meta-description', 'default meta description')">
+    <link rel="stylesheet" href="app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>   
 </head>
 <body>
     @include('partials.navigation')
 
     @yield('content')
     
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,6 +1,28 @@
-<nav>
-    <li><a href="{{route('welcome')}}">Inicio</a></li>
-    <li><a href="{{route('contact')}}">Contacto</a></li>
-    <li><a href="{{route('about')}}">Nosotros</a></li>
-    <li><a href="{{route('blog')}}">Blog</a></li>
-</nav>
+<nav class="navbar navbar-expand-md bg-body-tertiary border-3 border-bottom border-success">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">PetFinder</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-success" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-success" href="#">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-success" href="#">Contacto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Adopta</a>
+          </li>
+        </ul>
+        <div class="container">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </div>       
+      </div>
+    </div>
+  </nav>
