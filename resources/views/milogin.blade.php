4a76c44657ee74efb2cb3f8e95571201561c39b4
cesar rihu <cesarrihu@hotmail.com>
2023-10-12 15:41:52 -0600

    cambios finales

--- resources/views/milogin.blade.php
+++ resources/views/milogin.blade.php
@@ -37,7 +37,7 @@
 
                 <div class="mb-4">
                     <label for="email" class="form-label" value="{{ old('email') }}">correo electronico</label>
-                    <input type="email" placeholder="usuario@ejemplo.com" class="form-control bg-success-subtle" name="email" :value="old('email')" required autofocus>
+                    <input type="email" placeholder="usuario@ejemplo.com" class="form-control bg-success-subtle" name="email" value="{{old('email')}}" required autofocus>
                 </div>
                 <div class="mb-4">
                     <label for="password" class="form-label" value="{{ old('password') }}">contraseña</label>
