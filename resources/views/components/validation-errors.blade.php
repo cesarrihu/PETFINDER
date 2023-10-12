b418856209da87554de54646c2befee363f8066e
cesar rihu <cesarrihu@hotmail.com>
2023-10-12 15:20:34 -0600

    agregue la traduccion de las validaciones

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,8 +1,8 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="font-medium text-red-600">{{ __('Caracoles! Algo salio mal.') }}</div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        <ul class="alert alert-danger mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
